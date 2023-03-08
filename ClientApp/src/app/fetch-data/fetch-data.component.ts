09fc55bb6bdeb14c030e5cec8af05e794415d465
skebis <keranen.emil@gmail.com>
2023-03-08 13:45:59 +0200

    Added simple form and input fields for api testing.

--- ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -7,12 +7,33 @@ import { HttpClient } from '@angular/common/http';
 })
 export class FetchDataComponent {
   public recipes: Recipe[] = [];
+  public ingredients: Ingredient[] = [];
   public baseApiUrl: string = "api/";
+  public apiAddRecipe: string = "recipe";
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<Recipe[]>(baseUrl + this.baseApiUrl + 'recipe').subscribe(result => {
+    http.get<Recipe[]>(baseUrl + this.baseApiUrl + this.apiAddRecipe).subscribe(result => {
       this.recipes = result;
     }, error => console.error(error));
+    
+  }
+
+  ngOnInit() {
+    // TODO: move api calls here
+  }
+  ngOnDestroy() {
+    // Unsubscribes and such
+  }
+
+  public AddRecipe() {
+    console.log("add recipe works!");
+  }
+  public AddNewIngredient() {
+    this.ingredients.push({
+        name: 'test ingredient',
+        amount: 5,
+        unit: 'kg'
+    });
   }
 }
 
