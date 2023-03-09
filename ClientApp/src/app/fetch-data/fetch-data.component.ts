e046885bca57b819efc265066a7c86abbf36ed6f
Emil Keränen <emalkera@jyu.fi>
2023-03-09 17:49:34 +0200

    Api post method test.

--- ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,32 +1,50 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-fetch-data',
   templateUrl: './fetch-data.component.html'
 })
+
+@Injectable()
 export class FetchDataComponent {
   public recipes: Recipe[] = [];
   public ingredients: Ingredient[] = [];
   public baseApiUrl: string = "api/";
-  public apiAddRecipe: string = "recipe";
+  public apiGetRecipe: string = "recipe";
+  public apiPostRecipe: string = "recipe";
+  public recipe: Recipe = {
+    name: 'uusi resepti',
+    ingredients: [{
+      name: 'test ingredient',
+      amount: 5,
+      unit: 'kg'
+    }],
+    description: 'tässä uusi hieno resepti'
+  };
+  
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<Recipe[]>(baseUrl + this.baseApiUrl + this.apiAddRecipe).subscribe(result => {
-      this.recipes = result;
-    }, error => console.error(error));
-    
+  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
   }
 
   ngOnInit() {
-    // TODO: move api calls here
+    this.http.get<Recipe[]>(this.baseUrl + this.baseApiUrl + this.apiGetRecipe).subscribe(result => {
+      this.recipes = result;
+    }, error => console.error(error));
   }
+
   ngOnDestroy() {
     // Unsubscribes and such
   }
 
   public AddRecipe() {
-    console.log("add recipe works!");
+    
+    this.http.post<Recipe>(this.baseUrl + this.baseApiUrl + this.apiPostRecipe,
+      this.recipe
+      ).subscribe(result => {
+      console.log(result);
+      }, error => console.error(error));
+    this.recipes.push(this.recipe);
   }
   public AddNewIngredient() {
     this.ingredients.push({
